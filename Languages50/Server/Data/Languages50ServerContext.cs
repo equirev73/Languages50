9803291f3af1c54f4fc63f23875946b5d6952eeb
Usuario <equi_rev_73@hotmail.com.ar>
2021-01-02 17:44:59 -0300

    Agregar archivos de proyecto.

---
+++ Languages50/Server/Data/Languages50ServerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Languages50.Shared;
+
+namespace Languages50.Server.Data
+{
+    public class Languages50ServerContext : DbContext
+    {
+        public Languages50ServerContext (DbContextOptions<Languages50ServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Languages50.Shared.ProgrammingLanguage> ProgrammingLanguage { get; set; }
+    }
+}
---
+++ Languages50/Server/Migrations/20210102195609_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Languages50.Server.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProgrammingLanguage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WebSite = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Extension = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgrammingLanguage", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProgrammingLanguage");
+        }
+    }
+}
